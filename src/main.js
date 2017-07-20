63c559ecaff22211e2d4999b4ab5b6f6db7a7c27
mrsteele <matt@omnionline.us>
2017-07-20 10:22:20 -0400

    small changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-const { TEST } = process.env
+const { TEST, TEST2 } = process.env
 
 document.querySelector('body').innerHTML = `
   Structured: ${process.env.TEST}<br />
-  Destructured: ${TEST}
+  Destructured: ${TEST}<br />
+  <hr />
+  Structured: ${process.env.TEST2}<br />
+  Destructured: ${TEST2}<br />
 `
