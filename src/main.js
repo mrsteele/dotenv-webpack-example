eece07846cb6f365c3f870d103f8486c21811557
mrsteele <matt@omnionline.us>
2017-09-07 09:12:47 -0400

    chore: Adding webpack-dev-server testing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-const { TEST, TEST2 } = process.env
+// const { TEST, TEST2 } = process.env
+//
+// document.querySelector('body').innerHTML = `
+//   Structured: ${process.env.TEST}<br />
+//   Destructured: ${TEST}<br />
+//   <hr />
+//   Structured: ${process.env.TEST2}<br />
+//   Destructured: ${TEST2}<br />
+// `
 
 document.querySelector('body').innerHTML = `
-  Structured: ${process.env.TEST}<br />
-  Destructured: ${TEST}<br />
-  <hr />
-  Structured: ${process.env.TEST2}<br />
-  Destructured: ${TEST2}<br />
+  env: ${process.env.TEST}<br />
+  define: ${process.env.SUPERTEST} hhh<br />
 `
--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,36 @@ var webpack = require('webpack')
 var Dotenv = require('dotenv-webpack')
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'src'),
-                query: {
-                  presets: 'es2015',
-                },
-            }
-        ]
-    },
-    plugins: [
-        // new webpack.NoEmitOnErrorsPlugin(),
-        new Dotenv({
-          path: '.env.local',
-          systemvars: true
-        })
-    ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map'
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: path.join(__dirname, 'src'),
+        query: {
+          presets: 'es2015'
+        }
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.NoEmitOnErrorsPlugin(),
+    new Dotenv({
+      path: '.env.local',
+      systemvars: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.SUPERTEST': JSON.stringify('This is a test')
+    })
+  ],
+  stats: {
+    // Nice colored output
+    colors: true
+  },
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map'
 }
